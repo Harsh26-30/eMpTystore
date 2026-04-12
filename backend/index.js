81fd2c2de84ae889a53bb3fae13143e096269bc1
Harsh <harshkittu10@gmail.com>
2026-04-12 13:50:11 +0530

    Fixing Reload issued

--- backend/index.js
+++ backend/index.js
@@ -29,10 +29,10 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.get(/. */, (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend/index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend/dist/index.html"));
 });
 
 
