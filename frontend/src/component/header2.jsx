01d3e5629c3cdf55b5662d8d647c509f3f098557
Harsh <harshkittu10@gmail.com>
2026-04-12 12:04:14 +0530

    Fixing image iisued

--- frontend/src/component/header2.jsx
+++ frontend/src/component/header2.jsx
@@ -7,7 +7,7 @@ const header2 = () => {
   const navigate = useNavigate();
   return (
     <div id='mainboxheader2'>
-      <img src="\public\E.png" alt="logo" />
+      <img src="\E.png" alt="logo" />
       <button onClick={(e) => { navigate("/Menu"); }}>
         <img src="\src\assets\menu_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="More" />
       </button>
