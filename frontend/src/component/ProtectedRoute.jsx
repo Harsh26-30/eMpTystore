4e33b9032fdd718a12ecb1c3eb17825c1cb69051
Harsh Raj <harshkittu10@gmail.com>
2026-06-01 18:03:47 +0530

    Working with share a profile

--- frontend/src/component/ProtectedRoute.jsx
+++ frontend/src/component/ProtectedRoute.jsx
@@ -1,10 +1,19 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
+  const location = useLocation();
 
   if (!token) {
-    return <Navigate to="/" />;
+    return (
+      <Navigate
+        to="/"
+        state={{
+          redirectTo: location.pathname
+        }}
+        replace
+      />
+    );
   }
 
   return children;
