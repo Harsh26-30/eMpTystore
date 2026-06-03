7ed37f8755e06af5c6722aa288bf0f632947ffc6
Harsh Raj <harshkittu10@gmail.com>
2026-06-03 16:52:48 +0530

    Working with Ui of website

--- frontend/src/component/UI.jsx
+++ frontend/src/component/UI.jsx
@@ -157,7 +157,7 @@ const UI = () => {
 
     updatedHTML = updatedHTML.replaceAll(
       "{{Productprice1id}}",
-      data?.productbox1?.productname || "Productprice1id"
+      data?.productbox1?.productname || "0"
     );
 
 
