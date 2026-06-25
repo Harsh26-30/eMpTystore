2dc35a7367e6f04db446a40f967253230dbfe29b
Harsh Raj <harshkittu10@gmail.com>
2026-06-25 18:10:33 +0530

    qr of delivery partner and scannner

--- frontend/src/component/QrScanner.jsx
+++ frontend/src/component/QrScanner.jsx
@@ -2,52 +2,56 @@ import { Html5Qrcode } from "html5-qrcode";
 import { useEffect, useRef } from "react";
 
 const QrScanner = ({ onScan }) => {
-  const scannerRef = useRef(null);
+    const scannerRef = useRef(null);
 
-  useEffect(() => {
-    const qr = new Html5Qrcode("reader");
-    scannerRef.current = qr;
+    useEffect(() => {
+        const qr = new Html5Qrcode("reader");
+        scannerRef.current = qr;
 
-    const start = async () => {
-      try {
-        await qr.start(
-  { facingMode: "environment" }, // try back camera
-  {
-    fps: 10,
-    qrbox: { width: 250, height: 250 },
-  },
-  (text) => {
-    console.log("SCAN:", text);
-    onScan(text);
-    qr.stop();
-  }
-);
-      } catch (e) {
-        console.log("START ERROR:", e);
-      }
-    };
+        const start = async () => {
+            try {
+                const devices = await Html5Qrcode.getCameras();
 
-    start();
+                if (devices && devices.length) {
+                    await qr.start(
+                        devices[0].id,
+                        {
+                            fps: 10,
+                            qrbox: { width: 250, height: 250 },
+                        },
+                        (text) => {
+                            console.log("SCAN:", text);
+                            onScan(text);
+                            qr.stop();
+                        }
+                    );
+                }
+            } catch (e) {
+                console.log("START ERROR:", e);
+            }
+        };
 
-    return () => {
-      if (scannerRef.current) {
-        scannerRef.current.stop().catch(() => {});
-      }
-    };
-  }, []);
+        start();
 
-  return (
-    <div
-      id="reader"
-      style={{
-        width: "100%",
-        maxWidth: "400px",
-        height: "400px",
-        margin: "auto",
-        background: "black",
-      }}
-    />
-  );
+        return () => {
+            if (scannerRef.current) {
+                scannerRef.current.stop().catch(() => { });
+            }
+        };
+    }, []);
+
+    return (
+        <div
+            id="reader"
+            style={{
+                width: "100%",
+                maxWidth: "400px",
+                height: "400px",
+                margin: "auto",
+                background: "black",
+            }}
+        />
+    );
 };
 
 export default QrScanner;
