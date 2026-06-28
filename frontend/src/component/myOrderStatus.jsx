7a8b7ced295794f7a6a3e381bb6fe320195d0df2
Harsh Raj <harshkittu10@gmail.com>
2026-06-28 14:13:31 +0530

    fixxing issed and remove image in order status

--- frontend/src/component/myOrderStatus.jsx
+++ frontend/src/component/myOrderStatus.jsx
@@ -40,14 +40,20 @@ const MyOrderStatus = () => {
       <Header2 />
       {orders.map((order) => (
         <div key={order._id} id='boxmyorderstatus'>
-          <div id='box1myorderstatus'>
-            <img src={order.productImage} alt="Product Image" />
-          </div>
           <div id='orderdetailmyorderstatus'>
-            <h3>Odered Item: {order.productname || "Not mentioned"}</h3>
-            <h3>Seller Name: {order.sellerOrShopName || "Not mentioned"}</h3>
+            {order.items?.map((item, index) => (
+              <h3 key={index}>
+                Ordered Item: {item.productname || "Not mentioned"}
+              </h3>
+            ))}            <h3>Seller Name: {order.sellerOrShopName || "Not mentioned"}</h3>
             <h3>Customer Name: {order.customername || "Not mentioned"}</h3>
             <h3>Customer Contact: {order.customerContact || "Not mentioned"}</h3>
+            <h3>
+              Total Amount: ₹
+              {order.items?.reduce((sum, item) => {
+                return sum + (item.price * item.quantity);
+              }, 0) || 0}
+            </h3>
           </div>
           <h3>Order Status</h3>
           <div style={{
@@ -55,21 +61,21 @@ const MyOrderStatus = () => {
               'grey' : order.orderStatus === 'Confirm' ?
                 'green' : order.orderStatus === 'preparing' ?
                   'blue' : order.orderStatus === 'OFD' ? 'Yellow'
-                    : order.orderStatus === 'Reached' ? 'green' :''
+                    : order.orderStatus === 'Reached' ? 'green' : ''
           }}
             id='orderstatusmyorderstatus'>
             <h4 style={{
               color: order.orderStatus === 'pending' ?
                 'grey' : order.orderStatus === 'Confirm' ?
                   'darkgreen' : order.orderStatus === 'preparing' ?
-                    'darkblue' : order.orderStatus === 'OFD' ? 'orange' 
-                    : order.orderStatus === 'Reached' ? 'darkgreen' :''
+                    'darkblue' : order.orderStatus === 'OFD' ? 'orange'
+                      : order.orderStatus === 'Reached' ? 'darkgreen' : ''
             }} >{order.orderStatus === 'Pending' ? "Pending" :
-             order.orderStatus === 'Confirm' ? "Preparing" :
-              order.orderStatus === 'RFD' ? "Ready For Delivery" :
-               order.orderStatus === 'OFD' ? "Out for Delivery" : 
-               order.orderStatus === 'Reached' ? "Reached" :
-                "Analysing..."}</h4>
+              order.orderStatus === 'Confirm' ? "Preparing" :
+                order.orderStatus === 'RFD' ? "Ready For Delivery" :
+                  order.orderStatus === 'OFD' ? "Out for Delivery" :
+                    order.orderStatus === 'Reached' ? "Reached" :
+                      "Analysing..."}</h4>
           </div>
 
           {order.orderStatus !== 'Reached' && <button id='qrbutton' style={{ width: "50%" }} onClick={() => { setQrVusibility(true), setSelectedOrder(order) }}>
